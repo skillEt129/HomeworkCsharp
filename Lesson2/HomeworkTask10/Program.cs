695e51f03ddc0b983bafad8f3e7bf9dff79dec63
Roman <roma-popov-98@inbox.ru>
2022-08-11 10:56:19 +0400

    Lesoon2, Commmit1

---
+++ Lesson2/HomeworkTask10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine(number);
+string numberText = Convert.ToString(number);
+if (numberText.Length > 2)
+{
+    Console.WriteLine($"{numberText[1]}");
+}
---
+++ Lesson2/HomeworkTask15/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1) 
+{Console.WriteLine ("нет") ; 
+
+}
+if (number == 2) 
+{Console.WriteLine ("нет") ; 
+
+}
+if (number == 3) 
+{Console.WriteLine ("нет") ; 
+
+}
+if (number == 4) 
+{Console.WriteLine ("нет") ; 
+
+}
+if (number == 5) 
+{Console.WriteLine ("нет") ; 
+
+}
+if (number == 6) 
+{Console.WriteLine ("да") ; 
+
+}
+if (number == 7) 
+{Console.WriteLine ("да") ; 
+
+}
