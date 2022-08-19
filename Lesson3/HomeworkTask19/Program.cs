bc43ef55df2c8f1583d14cbd678b9e098c07c961
Roman <roma-popov-98@inbox.ru>
2022-08-19 11:52:27 +0400

    Lesson 3, Commit 1

---
+++ Lesson3/HomeworkTask19/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+Console.Write("Введите пятизначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+void Palindrome(int num)
+{
+    Console.Write(num + " -> ");
+    int xa = num;
+    int xb = 0;
+    while (num > 0)
+    {
+        xb = xb * 10 + num % 10;
+        num = num / 10;
+
+    }
+    if (xa == xb)
+        Console.Write("Палиндром");
+    else
+        Console.Write("Не палиндром");
+}
+Palindrome(num);
---
+++ Lesson3/HomeworkTask21/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координату по оси Х точки А: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату по оси Y точки А: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату по оси Y точки А: ");
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату по оси Х точки B: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату по оси Y точки B: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату по оси Y точки А: ");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+// D=sqrt((X2-X1)^2 + (Y2-Y1)^2 + (Z2 - Z1)^2).
+double distance = Math.Sqrt(Math.Pow((xB - xA), 2) + Math.Pow((yB - yA), 2) + Math.Pow((zB - zA), 2));
+
+Console.WriteLine($"distance = {Math.Round(distance, 2)}");
