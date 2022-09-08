e014dcddde3581348a70204efcdcdff04f5105e8
Roman <roma-popov-98@inbox.ru>
2022-09-08 10:37:25 +0400

    Lesson 9, Commit 1

---
+++ Lesson9/HomeworkTask64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8"
+
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+/// start = 1, откуда начинаем печать чисел; end - N, конец нашей последовательности
+string PrintNumbers(int start, int end)
+{
+// Базовый случай
+if (start == end) return start.ToString(); // N = 5; start == 5, "5"
+// Рекурсивный случай
+return (start + ", " + PrintNumbers(start + 1, end));
+}
+Console.WriteLine($"Числа от {M} до {N}: {PrintNumbers(M, N)}");
---
+++ Lesson9/HomeworkTask66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int m, int n, int sum)
+{
+    sum = n;
+    if (m == n)
+        return sum;
+    return (sum + SumNumber(m, n - 1, sum));
+}
+Console.WriteLine($"{SumNumber(m, n, 0)}");
